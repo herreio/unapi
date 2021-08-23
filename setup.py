fe7f672f2a61e6fd21e032db1cb1de7c5e7574c9
herre <donatus.herre@slub-dresden.de>
2021-08-23 13:56:23 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="unapi",
-    version="0.1.4",
+    version="0.2.0",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="unAPI Client",
