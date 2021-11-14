2f6a5521b32d2f18c3c760e19bc31362ad6a929b
herre <donatus.herre@slub-dresden.de>
2021-11-14 18:04:48 +0100

    Update holding methods of PicaJson

--- unapi/parse/picaj.py
+++ unapi/parse/picaj.py
@@ -109,134 +109,283 @@ class PicaJson(SerialJson):
         """
         return self.get_value("045R", "a", unique=False, repeat=False, collapse=collapse)
 
-    def get_holdings_ppn(self):
+    def get_holdings_ppn(self, occurence="01"):
         """
         203@/7800: EPNs der Exemplardaten
         """
-        return self.get_value("203@", "0", repeat=False)
+        return self.get_value("203@", "0", occurence=occurence, repeat=False)
 
-    def get_holdings_ilns(self):
+    def get_holdings_ilns(self, occurence=None):
         """
         101@: ILNs der Exemplardaten
         """
-        return self.get_value("101@", "a", repeat=False)
+        return self.get_value("101@", "a", occurence=occurence, repeat=False)
 
-    def get_holdings_first_entry_date(self):
+    def get_holdings_isil(self, occurence="01"):
+        """
+        209A/7100: Signatur (Exemplardaten)
+          $B    Sigel (nur SWB)
+
+        Das Unterfeld $B wird bei SWB-Bibliotheken im ersten Signaturfeld maschinell belegt.
+        """
+        return self.get_value("209A", "B", occurence=occurence, repeat=False)
+
+    def get_holdings_isil_occurence(self, occurence="01"):
+        """
+        209A/7100: Signatur (Exemplardaten)
+
+        Das Unterfeld $B wird bei SWB-Bibliotheken im ersten Signaturfeld maschinell belegt.
+        """
+        codes = self.get_holdings_isil(occurence=occurence)
+        if codes is not None:
+            return len(codes)
+        else:
+            return 0
+
+    def get_holdings_isil_index(self, isil, occurence="01"):
+        """
+        209A/7100: Signatur (Exemplardaten)
+          $B    Sigel (nur SWB)
+
+        Das Unterfeld $B wird bei SWB-Bibliotheken im ersten Signaturfeld maschinell belegt.
+        """
+        codes = self.get_holdings_isil(occurence=occurence)
+        index = [i for i, c in enumerate(codes) if c == isil]
+        if len(index) > 0:
+            return index
+
+    def get_holdings_from_isil(self, isil, occurence="01"):
+        """
+        203@/7800: EPNs der Exemplardaten
+        209A/7100: Signatur (Exemplardaten)
+            $B    Sigel (nur SWB)
+        """
+        index = self.get_holdings_isil_index(isil, occurence=occurence)
+        if index is not None:
+            epns = self.get_holdings_ppn(occurence=occurence)
+            if len(epns) == self.get_holdings_isil_occurence(occurence=occurence):
+                holdings = []
+                for i in index:
+                    holdings.append(epns[i])
+                if len(holdings) > 0:
+                    return holdings
+            else:
+                logger.error("{0}: Unequal number of holding ISILs and PPNs".format(self.name))
+
+    def get_holdings_first_entry_date(self, occurence="01"):
         """
         201A/7902: Datum der Ersterfassung (Exemplardaten)
         """
-        return self.get_value("201A", "0", repeat=False)
+        return self.get_value("201A", "0", occurence=occurence, repeat=False)
 
-    def get_holdings_first_entry_date_date(self):
+    def get_holdings_first_entry_date_date(self, occurence="01"):
         """
         201A/7902: Datum der Ersterfassung (Exemplardaten)
         """
         first_entry_date_objs = []
-        first_entry_dates = self.get_holdings_first_entry_date()
+        first_entry_dates = self.get_holdings_first_entry_date(occurence=occurence)
         for first_entry_date in first_entry_dates:
             first_entry_date_objs.append(datetime.datetime.strptime(first_entry_date, "%d-%m-%y").date())
         return first_entry_date_objs
 
-    def get_holdings_first_entry_date_iso(self):
+    def get_holdings_first_entry_date_iso(self, occurence="01"):
         """
         201A/7902: Datum der Ersterfassung (Exemplardaten)
         """
         first_entry_date_iso = []
-        first_entry_dates = self.get_holdings_first_entry_date()
+        first_entry_dates = self.get_holdings_first_entry_date(occurence=occurence)
         for first_entry_date in first_entry_dates:
             first_entry_date_iso.append(datetime.datetime.strptime(first_entry_date, "%d-%m-%y").date().isoformat())
         return first_entry_date_iso
 
-    def get_holdings_latest_change_date(self):
+    def get_holdings_latest_change_date(self, occurence="01"):
         """
         201B/7903: Datum und Uhrzeit der letzten Änderung (Exemplardaten)
         """
-        return self.get_value("201B", "0", repeat=False)
+        return self.get_value("201B", "0", occurence=occurence, repeat=False)
 
-    def get_holdings_latest_change_time(self):
+    def get_holdings_latest_change_time(self, occurence="01"):
         """
         201B/7903: Datum und Uhrzeit der letzten Änderung (Exemplardaten)
         """
-        return self.get_value("201B", "t", repeat=False)
+        return self.get_value("201B", "t", occurence=occurence, repeat=False)
 
-    def get_holdings_latest_change_str(self):
+    def get_holdings_latest_change_str(self, occurence="01"):
         """
         201B/7903: Datum und Uhrzeit der letzten Änderung (Exemplardaten)
         """
         latest_change_str = []
-        latest_change_date = self.get_holdings_latest_change_date()
-        latest_change_time = self.get_holdings_latest_change_time()
+        latest_change_date = self.get_holdings_latest_change_date(occurence=occurence)
+        latest_change_time = self.get_holdings_latest_change_time(occurence=occurence)
         if len(latest_change_date) != len(latest_change_time):
             logger.error("{0}: Unequal number of edit dates and times in holding data!".format(self.name))
             return None
         for i in range(len(latest_change_date)):
             latest_change_str.append("{0} {1}".format(latest_change_date[i], latest_change_time[i]))
-        return latest_change_str
+        if len(latest_change_str) > 0:
+            return latest_change_str
 
-    def get_holdings_latest_change_datetime(self):
+    def get_holdings_latest_change_datetime(self, occurence="01"):
         """
         201B/7903: Datum und Uhrzeit der letzten Änderung (Exemplardaten) (as datetime object)
         """
         latest_change_datetime = []
-        change_str = self.get_holdings_latest_change_str()
+        change_str = self.get_holdings_latest_change_str(occurence=occurence)
         for ch_str in change_str:
             if ch_str is not None:
                 latest_change_datetime.append(datetime.datetime.strptime(ch_str, "%d-%m-%y %H:%M:%S.%f"))
             else:
                 latest_change_datetime.append(ch_str)
-        return latest_change_datetime
+        if len(latest_change_datetime) > 0:
+            return latest_change_datetime
 
-    def get_holdings_latest_change_iso(self):
+    def get_holdings_latest_change_iso(self, occurence="01"):
         """
         201B/7903: Datum und Uhrzeit der letzten Änderung (Exemplardaten) (in ISO format)
         """
         latest_change_iso = []
-        change_str = self.get_holdings_latest_change_str()
+        change_str = self.get_holdings_latest_change_str(occurence=occurence)
         for ch_str in change_str:
             if ch_str is not None:
                 latest_change_iso.append(datetime.datetime.strptime(ch_str, "%d-%m-%y %H:%M:%S.%f").isoformat())
             else:
                 latest_change_iso.append(ch_str)
-        return latest_change_iso
+        if len(latest_change_iso) > 0:
+            return latest_change_iso
 
-    def get_holdings_source_first_entry(self):
+    def get_holdings_isil_latest_change_str(self, isil, occurence="01"):
+        """
+        201B/7903: Datum und Uhrzeit der letzten Änderung (Exemplardaten)
+        """
+        index = self.get_holdings_isil_index(isil)
+        if index is not None:
+            isil_latest_change_str = []
+            latest_change_str = self.get_holdings_latest_change_str(occurence=occurence)
+            for i in index:
+                isil_latest_change_str.append(latest_change_str[i])
+            if len(isil_latest_change_str) > 0:
+                return isil_latest_change_str
+
+    def get_holdings_isil_latest_change_datetime(self, isil, occurence="01"):
+        """
+        201B/7903: Datum und Uhrzeit der letzten Änderung (Exemplardaten) (as datetime object)
+        """
+        change_str = self.get_holdings_isil_latest_change_str(isil, occurence=occurence)
+        if change_str is not None:
+            latest_change_datetime = []
+            for ch_str in change_str:
+                latest_change_datetime.append(datetime.datetime.strptime(ch_str, "%d-%m-%y %H:%M:%S.%f"))
+            if len(latest_change_datetime) > 0:
+                return latest_change_datetime
+
+    def get_holdings_isil_latest_change_iso(self, isil, occurence="01"):
+        """
+        201B/7903: Datum und Uhrzeit der letzten Änderung (Exemplardaten) (in ISO format)
+        """
+        change_str = self.get_holdings_isil_latest_change_str(isil, occurence=occurence)
+        if change_str is not None:
+            latest_change_iso = []
+            for ch_str in change_str:
+                latest_change_iso.append(datetime.datetime.strptime(ch_str, "%d-%m-%y %H:%M:%S.%f").isoformat())
+            if len(latest_change_iso) > 0:
+                return latest_change_iso
+
+    def get_holdings_source_first_entry(self, occurence="01"):
         """
         201D/7901: Quelle und Datum der Ersterfassung (Exemplardaten)
         """
-        return self.get_value("201D", "0", repeat=False)
+        return self.get_value("201D", "0", occurence=occurence, repeat=False)
 
-    def get_holdings_source_first_entry_eln(self):
+    def get_holdings_source_first_entry_eln(self, occurence="01"):
         """
         201D/7901: Quelle der Ersterfassung (Exemplardaten)
         """
         codes = []
-        source_first_entry = self.get_holdings_source_first_entry()
+        source_first_entry = self.get_holdings_source_first_entry(occurence=occurence)
         for sfe in source_first_entry:
             codes.append(sfe.split(":")[0])
         return codes
 
-    def get_holdings_source_first_entry_date(self):
+    def get_holdings_source_first_entry_date(self, occurence="01"):
         """
         201D/7901: Datum der Ersterfassung (Exemplardaten)
         """
         dates = []
-        source_first_entry = self.get_holdings_source_first_entry()
+        source_first_entry = self.get_holdings_source_first_entry(occurence=occurence)
         for sfe in source_first_entry:
             dates.append(sfe.split(":")[1])
-        return dates
+        if len(dates) > 0:
+            return dates
 
-    def get_holdings_source_first_entry_date_date(self):
+    def get_holdings_source_first_entry_date_date(self, occurence="01"):
         """
         201D/7901: Datum der Ersterfassung (Exemplardaten) (as date object)
         """
-        first_entry_date = self.get_holdings_source_first_entry_date()
+        dates = []
+        first_entry_date = self.get_holdings_source_first_entry_date(occurence=occurence)
         if first_entry_date is not None:
-            return datetime.datetime.strptime(first_entry_date, "%d-%m-%y").date()
+            for sfe_date in first_entry_date:
+                dates.append(datetime.datetime.strptime(sfe_date, "%d-%m-%y").date())
+        if len(dates) > 0:
+            return dates
 
-    def get_holdings_source_first_entry_date_iso(self):
+    def get_holdings_source_first_entry_date_iso(self, occurence="01"):
         """
         201D/7901: Quelle und Datum der Ersterfassung (Exemplardaten) (in ISO format)
         """
-        first_entry_date = self.get_holdings_source_first_entry_date_date()
+        dates = []
+        first_entry_date = self.get_holdings_source_first_entry_date_date(occurence=occurence)
         if first_entry_date is not None:
-            first_entry_date.isoformat()
+            for sfe_date in first_entry_date:
+                dates.append(sfe_date.isoformat())
+        if len(dates) > 0:
+            return dates
+
+    def get_holdings_eln_first_entry(self, eln, occurence="01"):
+        """
+        201D/7901: Quelle und Datum der Ersterfassung (Exemplardaten)
+        """
+        elns = self.get_holdings_source_first_entry_eln(occurence=occurence)
+        if elns is not None and eln in elns:
+            eln_entries = []
+            entries = self.get_holdings_source_first_entry(occurence=occurence)
+            if entries is not None:
+                eln_entries = [e for e in entries if eln in e]
+            if len(eln_entries) > 0:
+                return eln_entries
+
+    def get_holdings_eln_first_entry_date(self, eln, occurence="01"):
+        """
+        201D/7901: Datum der Ersterfassung (Exemplardaten)
+        """
+        dates = []
+        source_first_entry = self.get_holdings_eln_first_entry(eln, occurence=occurence)
+        if source_first_entry is not None:
+            for sfe in source_first_entry:
+                dates.append(sfe.split(":")[1])
+            if len(dates) > 0:
+                return dates
+
+    def get_holdings_eln_first_entry_date_date(self, eln, occurence="01"):
+        """
+        201D/7901: Datum der Ersterfassung (Exemplardaten) (as date object)
+        """
+        dates = []
+        first_entry_date = self.get_holdings_eln_first_entry_date(eln, occurence=occurence)
+        if first_entry_date is not None:
+            for sfe_date in first_entry_date:
+                dates.append(datetime.datetime.strptime(sfe_date, "%d-%m-%y").date())
+        if len(dates) > 0:
+            return dates
+
+    def get_holdings_eln_first_entry_date_iso(self, eln, occurence="01"):
+        """
+        201D/7901: Quelle und Datum der Ersterfassung (Exemplardaten) (in ISO format)
+        """
+        dates = []
+        first_entry_date = self.get_holdings_eln_first_entry_date_date(eln, occurence=occurence)
+        if first_entry_date is not None:
+            for sfe_date in first_entry_date:
+                dates.append(sfe_date.isoformat())
+        if len(dates) > 0:
+            return dates
