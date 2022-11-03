30a0dea15f7118f1c9cbdf266057471c1a4d1f76
Donatus Herre <donatus.herre@slub-dresden.de>
2022-11-03 13:41:13 +0100

    Import __version__ in utils

--- unapi/utils.py
+++ unapi/utils.py
@@ -5,6 +5,8 @@ import logging
 import requests
 from lxml import etree
 
+from . import __version__
+
 
 def get_logger(loglevel=None):
     """
@@ -30,7 +32,7 @@ def get_request(url, headers={}):
     Send HTTP GET request to given URL.
     """
     if "User-Agent" not in headers:
-        headers["User-Agent"] = "py-unapi 0.5.2"
+        headers["User-Agent"] = "py-unapi {0}".format(__version__)
     try:
         return requests.get(url, headers=headers)
     except requests.exceptions.RequestException as err:
