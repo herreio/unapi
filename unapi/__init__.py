478ad78fa32c8c22c2ee15310d912f17eafa845c
Donatus Herre <donatus.herre@slub-dresden.de>
2022-11-03 13:41:28 +0100

    Bump version number

--- unapi/__init__.py
+++ unapi/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 __license__ = "GPLv3"
 
 from .client import Client
